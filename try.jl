f770a35a56ca7eb0c887e8e37b093b58efb123bf
Weng Wei <wuvist@gmail.com>
2024-03-31 20:37:50 +0800

    Update HK LCPC

--- try.jl
+++ try.jl
@@ -1,5 +1,9 @@
 using CSV, DataFrames, GLM, Plots
 
+c1 = theme_palette(:auto).colors.colors[1]
+c2 = theme_palette(:auto).colors.colors[2]
+c3 = theme_palette(:auto).colors.colors[3]
+
 # HES: REPORT ON THE HOUSEHOLD EXPENDITURE SURVEY, 2017/18
 # data/household_expenditure.pdf
 
@@ -64,24 +68,58 @@ savefig("data/LCPC_change.png")
 # Table 140-09003 : 2009/10 Household Expenditure Survey https://www.censtatd.gov.hk/en/web_table.html?id=140-09003
 # Table 140-09002 : 2004/05 Household Expenditure Survey https://www.censtatd.gov.hk/en/web_table.html?id=140-09002
 # Table 140-09001 : 1999/00 Household Expenditure Survey https://www.censtatd.gov.hk/en/web_table.html?id=140-09001
+hk_e_1920 = [15018, 27631, 37895]
+hk_e_1415 = [13275, 26075, 36728]
+hk_e_0910 = [10569, 19288, 28715]
 
+hk_rent_1920 = [1701, 10100, 15218]
+hk_rent_1415 = [1576, 8982, 13582]
+hk_rent_0910 = [1116, 5969, 9573]
 
 # Income https://www.censtatd.gov.hk/en/web_table.html?id=130-06613A
 # Table 130-06613A : Median monthly household income (excluding Chinese New Year bonus/double pay) by type of housing (excluding foreign domestic helpers)
 # "data/Table 130-06613A_en.csv"
-hk_e_1920 = [15018, 27631, 37895]
 
+# 2007,Q1,10500,19500,23000,17000
+# 2008,Q1,11000,20000,24000,17700
+# 2009,Q1,10600,20000,24000,17800
+# 2010,Q1,10500,19500,24500,17500
+# 2011,Q1,11000,20500,26000,18500
+# 2012,Q1,12000,23000,30000,20000
+# 2013,Q1,13000,23700,30000,21400
+# 2014,Q1,14000,24000,30100,22000
+# 2015,Q1,14100,26000,33000,23700
+# 2016,Q1,15000,26000,33700,24500
+# 2017,Q1,16000,27000,35000,25500
+# 2018,Q1,16900,29000,38000,27600
 # 2019,Q1,18500,29600,39000,28600
 # 2020,Q1,18000,27200,38800,28000
-hk_i_21 = [(18500 + 18000) / 2, (29600 + 27200) / 2, (39000 + 38800) / 2]
-hk_rent = [1701, 10100, 15218]
+# 2021,Q1,17400,25200,36000,26000
+# 2022,Q1,18400,27100,37000,27400
+# 2023,Q1,19500,27100,38700,28700
+hk_i_0910 = [(10600 + 10500) / 2, (20000 + 19500) / 2, (24000 + 24500) / 2]
+hk_i_1415 = [(14000 + 14100) / 2, (24000 + 26000) / 2, (30100 + 33000) / 2]
+hk_i_1920 = [(18500 + 18000) / 2, (29600 + 27200) / 2, (39000 + 38800) / 2]
 
-LCPC_hk = hk_e_1920 ./ hk_i_21
-LCPC_exclude_rent_hk = (hk_e_1920 .- hk_rent) ./ hk_i_21
-plot(LCPC_hk, xlim=[0.8, 3.2], ylim=[0, 1.3], xticks=([1, 2, 3],
-        ["Public housing", "Subsidised Housing", "Private Housing"]), label="2019/20",
+LCPC_hk_0910 = hk_e_0910 ./ hk_i_0910
+LCPC_hk_1415 = hk_e_1415 ./ hk_i_1415
+LCPC_hk_1920 = hk_e_1920 ./ hk_i_1920
+
+LCPC_hk_exclude_rent_0910 = (hk_e_0910 .- hk_rent_0910) ./ hk_i_0910
+LCPC_hk_exclude_rent_1415 = (hk_e_1415 .- hk_rent_1415) ./ hk_i_1415
+LCPC_hk_exclude_rent_1920 = (hk_e_1920 .- hk_rent_1920) ./ hk_i_1920
+
+
+plot(LCPC_hk_0910, xlim=[0.8, 3.2], ylim=[0, 1.3], linestyle=:dot, xticks=([1, 2, 3],
+        ["Public housing", "Subsidised Housing", "Private Housing"]), label="2009/10",
     title="HK LCPC by type of housing", ylabel="Monthly LCP(Expenditure/Income)", xlabel="Type of Housing")
-plot!(LCPC_exclude_rent_hk, label="2019/20 exclude rent")
+plot!(LCPC_hk_1415, label="2014/15", linestyle=:dash)
+plot!(LCPC_hk_1920, label="2019/20")
+
+plot!(LCPC_hk_exclude_rent_0910, label="2009/10 exclude rent", color=c1, linestyle=:dot)
+plot!(LCPC_hk_exclude_rent_1415, label="2014/15 exclude rent", color=c2, linestyle=:dash)
+plot!(LCPC_hk_exclude_rent_1920, label="2019/20 exclude rent", color=c3)
+
 savefig("data/LCPC_hk.png")
 
 # http://www.singstat.gov.sg/whats-new/latest-news/cpi-highlights
